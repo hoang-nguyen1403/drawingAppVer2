2eb9fbcd616415b6894cd8470fc2d85928665668
TrucPhuong <trucphuongnguyen751@gmail.com>
2022-10-23 16:06:49 +0700

    completed send picture

--- backend/api_basic/controllers/picture.py
+++ backend/api_basic/controllers/picture.py
@@ -1,53 +1,22 @@
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
-# from ..models.Article import Article
-# from ..serializers import ArticleSerializer
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from rest_framework import status
 import json
-import numpy as np
-import cv2
-from ..forms import UploadFrom
-from ..models.Article import Article
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.http import HttpResponse
-from rest_framework.response import Response
-
-
+from django.core.files.storage import default_storage
 class PictureAPIView(APIView):
-
-    # def index(self, request):
-    #     form = UploadFrom()
-    #     return render(request, 'article/index.html', {'form': form})
 
     def get(self, request):
          if request.method == 'GET':
-            picture = Article.objects.all()
             obj = json.load(open('UIp_FE.json'))
             return JsonResponse(obj, safe=False)
 
     def post(self, request):
          if request.method == 'POST':
-            img = request.data
-            # with open('data.txt', 'w') as f:
-            #    f.write(str(img))
-            # form= UploadFrom(request.POST.get('pic1'))
-            # if form.is_valid():
-            #    form.save()
-            # name = request.POST.get("name")
-            # url = request.POST.get("imgURL")
-            # caption = request.POST.get("caption")
-            # if form.is_valid():
-            #    form.save()
-            #    return HttpResponse('success')
-            # else:
-            #    form = UploadFrom()
-            #    return HttpResponse('no')
-         return HttpResponse('success')
+            file = request.FILES['file']
+            file_name = default_storage.save(file.name,file)
+            return JsonResponse(file_name,safe=False)
             
 
             
--- backend/api_basic/urls.py
+++ backend/api_basic/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from .controllers.picture import PictureAPIView
 from .controllers.article import ArticleAPIView
 from .controllers.image import saveFile
 
@@ -9,5 +9,6 @@ from .controllers.image import saveFile
 urlpatterns = [
     # path('article/', article_list),
     path('detectArea/',ArticleAPIView.as_view()),
-    path('upload/',saveFile),
+    # path('upload/',saveFile),
+    path('picture/',PictureAPIView.as_view()),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
