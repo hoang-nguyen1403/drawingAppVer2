14a040469bc3fc0a2cd5c2c7edc95c2fa72d928c
phuong Nguyen <trucphuongnguyen751@gmail.com>
2022-10-28 14:04:33 +0700

    update sent picture

--- backend/api_basic/urls.py
+++ backend/api_basic/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .controllers.article import ArticleAPIView
+from django.conf.urls.static import static
+from django.conf import settings
 from .controllers.picture import PictureAPIView
+from .controllers.article import ArticleAPIView
+# from .controllers.image import saveFile
+
 
 urlpatterns = [
     # path('article/', article_list),
-    path('detectArea/', ArticleAPIView.as_view()),
-    path('picture/', PictureAPIView.as_view()),
-    # path('', PictureAPIView.index, name='index'),
-]
+    path('detectArea/',ArticleAPIView.as_view()),
+    # path('upload/',saveFile),
+    path('picture/',PictureAPIView.as_view()),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
