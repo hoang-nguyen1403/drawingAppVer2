4685e8af7a3325baed136d5433cd40c8c615ee23
nguyenngocvy1 <vy070401@gmail.com>
2022-11-08 17:00:15 +0700

    try to deploy heroku

--- backend/MyProject/settings.py
+++ backend/MyProject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
